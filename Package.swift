1ae26d6fb4a2cb5001a163f545245d907b46ab16
{rnantes} <{reid.nantes@gmail.com}>
2021-03-13 22:43:23 -0500

    use fluent-kit main

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/async-kit.git", from: "1.2.0"),
         //.package(url: "https://github.com/vapor/fluent-kit.git", from: "1.0.0"),
-        .package(url: "https://github.com/rnantes/fluent-kit.git", .branch("master")),
+        .package(url: "https://github.com/rnantes/fluent-kit.git", .branch("main")),
         //.package(url: "https://github.com/vapor/postgres-kit.git", from: "2.0.0"),
         .package(url: "https://github.com/rnantes/postgres-kit.git", .branch("all-encoding-strategies"))
     ],
